b90984591d3f6e3fd73682a1cb593ed3e5513af2
Dee <darchdekin02@gmail.com>
2024-02-28 13:58:41 -0600

    Update AppNav.vue

--- frontend/src/components/AppNav.vue
+++ frontend/src/components/AppNav.vue
@@ -1,13 +1,16 @@
 <template>
-  <menubar>
+  <prime-toolbar>
     <template #end>
       <prime-button type="button" label="Sign Out" icon="pi pi-sign-out" @click="useAuthStore().unauthenticate()" />
     </template>
-  </menubar>
+    <template #start>
+      <router-link to="/"></router-link>
+    </template>
+  </prime-toolbar>
 </template>
 
 <script setup lang="ts">
-import Menubar from 'primevue/menubar';
+import PrimeToolbar from 'primevue/toolbar';
 import PrimeButton from 'primevue/button';
 import { useAuthStore } from '@/store';
 </script>
