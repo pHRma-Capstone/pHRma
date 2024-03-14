3106f5700013f867eaa283fa116633b7241a159e
naroop <nrupp815@outlook.com>
2024-03-14 13:24:22 -0500

    updated deployment
    - removed a lot of the workflow out of the github action and into the dockerfiles
    - backend now reverse proxied from nginx

--- frontend/src/util/api.ts
+++ frontend/src/util/api.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosInstance } from 'axios';
 
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? 'http://35.153.163.209:3000/api' : 'http://localhost:3000/api'
+  baseURL: process.env.NODE_ENV === 'production' ? 'http://localhost/api' : 'http://localhost:3000/api'
 });
 
 export default api;
