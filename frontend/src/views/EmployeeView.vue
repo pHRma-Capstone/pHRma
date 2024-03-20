7cfec4016ab065265bf3759e17ce434803b0d2c8
GalactiCow <gmc8yp@umsystem.edu>
2024-03-20 16:38:44 -0500

    Margin touchup

--- frontend/src/views/EmployeeView.vue
+++ frontend/src/views/EmployeeView.vue
@@ -1,7 +1,9 @@
 <template> 
   <!-- main chart -->
-  <div class="row-span-1 col-span-4 md:row-span-2 md:col-span-2 border rounded shadow-md flex flex-col">
-    <service-statistics :data="useServiceStatisticsStore().get()" />
+  <div class="p-10">
+    <div class="row-span-1 col-span-4 md:row-span-2 md:col-span-2 border rounded shadow-md flex flex-col">
+      <service-statistics :data="useServiceStatisticsStore().get()" />
+    </div>
   </div>
 </template>
 
