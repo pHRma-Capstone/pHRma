763bc7cdc266979466aa5ec1345361597479f6c9
naroop <nrupp815@outlook.com>
2024-04-02 10:31:31 -0500

    naming conventions

---
+++ backend/src/routes/employeeStatisticRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import db from '../db';
+import EmployeeStatisticsController from '../controllers/EmployeeStatisticController';
+import { EmployeeStatistic } from '../entities/EmployeeStatistic';
+
+const repository = db.getRepository(EmployeeStatistic);
+const controller = new EmployeeStatisticsController(repository);
+const router = Router();
+
+router.get('/employee-statistics', controller.getEmployeeStatistics.bind(controller));
+
+export default router;
