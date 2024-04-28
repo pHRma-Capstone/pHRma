f03cf9d1dcbb97abd027a5d511b12ae1fe8301e1
jamesgrady <zgrady@gmail.com>
2024-04-28 10:30:57 -0500

    fixed file upload route to just call controller function

--- backend/src/routes/fileUploadRoutes.ts
+++ backend/src/routes/fileUploadRoutes.ts
@@ -1,21 +1,15 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 import multer from 'multer';
+import FileUploadController from "../controllers/FileUploadController";
 
 const router = Router();
 
-const storage = multer.diskStorage({
-  destination: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, destination: string) => void) {
-    cb(null, 'uploads/');
-  },
-  filename: function (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) {
-    cb(null, new Date().toJSON().slice(0, -5) + '.csv');
-  }
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({ storage });
 
-router.post('/file-upload', upload.single('UploadedFile'), (req: Request, res: Response) => {
-  res.send('File uploaded');
-});
+const controller = new FileUploadController();
+
+router.post('/upload_files', controller.FileUpload.bind(controller));
 
 export default router;
